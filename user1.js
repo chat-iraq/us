c17484f6d95eadd376e85bc14dc5a50a1bdc810c
chat-iraq <79407256+chat-iraq@users.noreply.github.com>
2023-04-11 21:24:45 +0300

    Update and rename user.js to user1.js

--- user1.js
+++ user1.js
@@ -153,7 +153,7 @@ margin-left: -4px;
 var l_itvshihanh=setInterval(function(){
     if(myid!=null){
         clearInterval(l_itvshihanh);
-        $("#users .uzr:contains('Έsɾαα')").addClass('shihanh1');
+        $("#users .uzr:contains('Έsɾαα')").addClass('Έsɾαα1');
         $("#users .uzr.shihanh1 .fitimg.u-pic").append('<img class="shihanh_star1" src="https://up6.cc/2023/04/168123656220261.png">');
 		$("#users .uzr.shihanh1 span.u-topic").after('<img id="mystory1" class="My_vedio1" title="ستوري" style="float: right;width: 20px;margin-top: 0px;margin-right: 5px;cursor: pointer;" src="https://i.ibb.co/47VS4nY/image.png">');
         $("#users .uzr.shihanh1 span.uhash, #users .uzr.shihanh1 img.co").hide(); 
